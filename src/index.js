84c31bf944ef452d3f7085905632237e1731fe55
jyotiranjanpradhan <jrpliku331@gmail.com>
2024-05-10 23:34:42 +0530

    setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const {PORT}=require('./config/serverConfig')
+const setupAndStartServer=async ()=>{
+    const app=express();
+    app.listen(PORT,()=>{
+        console.log(`${PORT} started `);
+    })
+} 
+setupAndStartServer();
