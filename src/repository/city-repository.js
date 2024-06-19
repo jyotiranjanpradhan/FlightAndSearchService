c0d511cc00174134ba546ac0a3c028f3ac8d96cb
jyotiranjanpradhan <jrpliku331@gmail.com>
2024-06-19 16:59:15 +0530

    Complited City Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,8 +1,9 @@
 // src/repository/city-repository.js
-const { where } = require('sequelize');
-const {city}   = require('../models'); // Ensure correct path
+const { where } = require("sequelize");
+const { city } = require("../models"); // Ensure correct path
 
 class CityRepository {
+  //Create City
   async createCity({ name }) {
     try {
       const createdCity = await city.create({ name });
@@ -11,16 +12,42 @@ class CityRepository {
       console.log(error.name);
     }
   }
-  async DeleteCity({cityid}){
+
+  //Delete City
+  async DeleteCity(cityid) {
     try {
       await city.destroy({
-        where:{
-          id:cityid
-        }
-      })
+        where: {
+          id: cityid,
+        },
+      });
     } catch (error) {
       console.log(error);
-      
+    }
+  }
+
+  //Update City
+  async UpdateCity({cityid, name}) {
+    try {
+      const cityname = await city.update({name:name}, {
+        where: {
+          id: cityid,
+        },
+      });
+      return cityname;
+    } catch (error) {
+      console.log(error.name);
+    }
+  }
+
+  //Get City
+  async GetCity({cityid}) {
+    try {
+      const cityname=await city.findByPk(cityid);
+      return city;
+    } catch (error) {
+      console.log("some Error in Getting city ");
+      console.log(error.name);
     }
   }
 }
